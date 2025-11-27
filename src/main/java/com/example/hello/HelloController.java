30cc5fbe509bb10cdb6b15a38f84ebaee4cef76a
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-11-27 11:55:01 +0530

    Update HelloController.java

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello Welcome to demo Assigment 12 ";
+        return "Hello Welcome to demo Assigment 20 ";
     }
 }
 
